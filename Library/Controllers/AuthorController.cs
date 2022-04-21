f38d8eaecec2569f697ece8407eee1da717781e4
mervesiseci@gmail.com <mervesiseci@gmail.com>
2022-04-21 23:24:34 +0300

    21.04

---
+++ Library/Controllers/AuthorController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Library.Models.Entity;
+
+namespace Library.Controllers
+{
+    public class AuthorController : Controller
+    {
+        // GET: Author
+        DbLibraryEntities db = new DbLibraryEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.Author.ToList();
+            return View(degerler);
+        }
+    }
+}
--- Library/Controllers/CategoryController.cs
+++ Library/Controllers/CategoryController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Library.Models.Entity;
+
 namespace Library.Controllers
 {
     public class CategoryController : Controller
@@ -31,5 +32,29 @@ namespace Library.Controllers
             db.SaveChanges();  /* değişiklikleri kaydet */
             return View(); /* sayfayı geri döndürsün. */
         }
-    }
+
+        public ActionResult KategoriSil(int id) /* Id ye göre sildirdigimiz icin int prmt. aldı */
+        {
+            var category = db.Category.Find(id);
+            db.Category.Remove(category);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult KategoriGetir(int id)
+        {
+            var ktg = db.Category.Find(id);
+            return View("KategoriGetir", ktg); /* ktg den gelen değerle döndür */
+        
+        }
+
+        public ActionResult KategoriGuncelle(Category p)
+        {
+            var ktg = db.Category.Find(p.Id);
+            ktg.CategoryName = p.CategoryName;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+    }  
 }
